57cb86f16dfb3bfaece9e1b91cec21450bbf2434
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-01-19 15:22:11 +0000

    complete fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(number)
-  if number == 15
+  if number % 3 == 0 && number % 5 == 0
     "fizzbuzz"
   elsif number % 3 == 0
     "fizz"
   elsif number % 5 == 0
     "buzz"
-  elsif number == 4
+  else
     number
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,12 +8,15 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5 && 10)).to eq 'buzz'
   end
   it 'returns "fizzbuzz" when passed 15 ' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(15 && 30)).to eq 'fizzbuzz'
   end
   it 'returns 4 when passsed 4' do
     expect(fizzbuzz(4)).to eq 4
   end
   it 'return "fizz" when passed 36' do
     expect(fizzbuzz(36)).to eq 'fizz'
+  end
+  it 'returns 101 when passed 101' do
+    expect(fizzbuzz(101)).to eq 101
   end
 end
