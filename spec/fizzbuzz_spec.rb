6d60a5c4b3a69ba1474a4219fd9db67e822aaf17
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-01-19 14:02:55 +0000

    first buzz test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passes 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passes 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
 end
